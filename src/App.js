f93193da6454a6d9db13b3c607dd4b473a61eff4
İsmail Emre Güngör <ismailemre.gungor@gazi.edu.tr>
2022-11-23 01:14:00 +0300

    fix routing

--- src/App.js
+++ src/App.js
@@ -1,11 +1,19 @@
-import logo from "./logo.svg";
 import "./App.css";
+import { Routes, Route } from "react-router-dom";
+import Navbar from "./components/Navbar/Navbar";
+import Home from "./components/Home/Home";
+import User from "./components/User/User";
 import Post from "./components/Post/Post";
 
 function App() {
   return (
     <div className="App">
-      <Post />
+      <Navbar />
+      <Routes>
+        <Route path="/home" element={<Home />} />
+        <Route path="/users" element={<User />} />
+        <Route path="/posts" element={<Post />} />
+      </Routes>
     </div>
   );
 }
--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,16 +1,21 @@
 import React from "react";
 import { Link } from "react-router-dom";
 import Home from "../Home/Home";
+import User from "../User/User";
 
 function Navbar() {
   return (
     <div>
       <ul>
         <li>
-          <Link to="/home">Home</Link>
+          <Link to="/home">
+            <Home />
+          </Link>
         </li>
         <li>
-          <Link to={{ pathname: `users` }}>Users</Link>
+          <Link to={{ pathname: `users` }}>
+            <User />
+          </Link>
         </li>
       </ul>
     </div>
