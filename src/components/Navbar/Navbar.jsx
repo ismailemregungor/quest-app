020bb4b9a43db280c190a1e0b0c3b2d11cde8485
İsmail Emre Güngör <ismailemre.gungor@gazi.edu.tr>
2022-11-27 20:27:01 +0300

    update Navbar component

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,18 +1,41 @@
 import React from "react";
 import { Link } from "react-router-dom";
 import "./Navbar.css";
+import AppBar from "@mui/material/AppBar";
+import Box from "@mui/material/Box";
+import Toolbar from "@mui/material/Toolbar";
+import Typography from "@mui/material/Typography";
+import IconButton from "@mui/material/IconButton";
+import MenuIcon from "@mui/icons-material/Menu";
 
 function Navbar() {
   return (
     <div className="navbarContainer">
-      <ul>
-        <li>
-          <Link to="/home">Home</Link>
-        </li>
-        <li>
-          <Link to={{ pathname: `users` }}>User</Link>
-        </li>
-      </ul>
+      <Box sx={{ flexGrow: 1 }}>
+        <AppBar position="static">
+          <Toolbar>
+            <IconButton
+              size="large"
+              edge="start"
+              color="inherit"
+              aria-label="menu"
+              sx={{ mr: 2 }}
+            >
+              <MenuIcon />
+            </IconButton>
+            <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
+              <Link className="headerLink" to="/home">
+                Home
+              </Link>
+            </Typography>
+            <Typography variant="h6" component="div">
+              <Link className="headerLink" to={{ pathname: `/users/` }}>
+                User
+              </Link>
+            </Typography>
+          </Toolbar>
+        </AppBar>
+      </Box>
     </div>
   );
 }
