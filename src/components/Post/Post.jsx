fe70bde4d917a2daa87e368e316ed49e44e175f7
İsmail Emre Güngör <ismailemre.gungor@gazi.edu.tr>
2022-11-27 20:25:23 +0300

    update Post component

--- src/components/Post/Post.jsx
+++ src/components/Post/Post.jsx
@@ -1,11 +1,79 @@
-import React from "react";
+import * as React from "react";
+import { styled } from "@mui/material/styles";
+import Card from "@mui/material/Card";
+import CardHeader from "@mui/material/CardHeader";
+import CardContent from "@mui/material/CardContent";
+import CardActions from "@mui/material/CardActions";
+import Collapse from "@mui/material/Collapse";
+import Avatar from "@mui/material/Avatar";
+import IconButton from "@mui/material/IconButton";
+import Typography from "@mui/material/Typography";
+import { red } from "@mui/material/colors";
+import FavoriteIcon from "@mui/icons-material/Favorite";
 import "./Post.css";
+import CommentIcon from "@mui/icons-material/Comment";
+import { Link } from "react-router-dom";
 
-function Post({ title, text }) {
+function Post({ userId, title, body }) {
+  const ExpandMore = styled((props) => {
+    const { expand, ...other } = props;
+    return <IconButton {...other} />;
+  })(({ theme, expand }) => ({
+    transition: theme.transitions.create("transform", {
+      duration: theme.transitions.duration.shortest,
+    }),
+  }));
+
+  const [expanded, setExpanded] = React.useState(false);
+  const [liked, setLiked] = React.useState(false);
+
+  const handleExpandClick = () => {
+    setExpanded(!expanded);
+  };
+
+  const handleLikeClick = () => {
+    setLiked(!liked);
+  };
+
   return (
-    <div>
-      {title} {text}
+    <div className="postContainer">
+      <Card>
+        <CardHeader
+          avatar={
+            <Link className="headerLink" to={{ pathname: `/users/` + userId }}>
+              <Avatar sx={{ bgcolor: red[500] }} aria-label="recipe">
+                {userId}
+              </Avatar>
+            </Link>
+          }
+          title={title}
+        />
+        <CardContent>
+          <Typography variant="body2" color="text.secondary">
+            {body}
+          </Typography>
+        </CardContent>
+        <CardActions disableSpacing>
+          <IconButton aria-label="add to favorites" onClick={handleLikeClick}>
+            <FavoriteIcon style={liked ? { color: "red" } : null} />
+          </IconButton>
+          <ExpandMore
+            expand={expanded}
+            onClick={handleExpandClick}
+            aria-expanded={expanded}
+            aria-label="show more"
+          >
+            <CommentIcon />
+          </ExpandMore>
+        </CardActions>
+        <Collapse in={expanded} timeout="auto" unmountOnExit>
+          <CardContent>
+            <Typography></Typography>
+          </CardContent>
+        </Collapse>
+      </Card>
     </div>
   );
 }
+
 export default Post;
